0b4db88183d746462c9ecac4aa479a7f1d9ed11c
SouthHartman <lucas.hartman@ops.org>
2015-11-16 11:34:22 -0600

    added lesson 9 files

---
+++ Lesson 9/Exercises/9.1.1.py
@@ -0,0 +1,27 @@
+# john
+# period 5
+# 11/12/15
+
+# 9.1.1
+
+# this program will check if number is positive or not
+
+num1 = int(input("Give me a number:  "))
+
+if num1 > 0:
+    print ("Your number is positive")
+else:
+    print ("The number is negative")
+
+
+# 9.1.2
+
+# this program will check if number is positive or not
+
+num1 = int(input("Give me a number:  "))
+num2 = int(input("Give me a number:  "))
+
+if num1 > num2:
+    print ("The number %d is larger than %d." %(num1, num2))
+else:
+    print ("The number %d is less than %d." %(num1, num2))
---
+++ Lesson 9/Labs/Lab 9.1.py
@@ -0,0 +1,8 @@
+# lab 9.1
+
+num = int(input("give me a number: "))
+
+if num%13 == 0:
+    print ("Your number is divisible by 13")
+else:
+    print ("Your number is NOT divisible by 13")
